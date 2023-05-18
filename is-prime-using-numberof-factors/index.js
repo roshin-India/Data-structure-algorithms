e8d7a102fdf86e15216e81749e19658c061b4bcf
Roshin <roshinkrosh@gmail.com>
2023-05-18 21:28:02 +0530

    check a number prime or not

---
+++ is-prime-using-numberof-factors/index.js
@@ -0,0 +1,30 @@
+function sqrRootNotPefectSquare(n) {
+  let i = 1;
+  let ans = 0;
+  while (i * i <= n) {
+    ans = i;
+    i++;
+  }
+  return ans;
+}
+function is_prime(A) {
+  let count = 0;
+  let sRoot = sqrRootNotPefectSquare(A);
+  for (let i = 1; i <= sRoot; i++) {
+    if (A % i == 0) {
+      if (i != A / i) {
+        count = count + 2;
+      } else {
+        count = count + 1;
+      }
+    }
+  }
+  if (count == 2) {
+    return "prime";
+  } else {
+    return "not prime";
+  }
+}
+
+let res = is_prime(100);
+console.log("output:", res);
